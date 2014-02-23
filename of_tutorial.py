997c402a7000acbad36c8665003b82cc89a83647
Matt Welch <mattw0308@gmail.com>
2014-02-23 13:12:20 -0700

    Added verbose debug messages when functions launch

--- of_tutorial.py
+++ of_tutorial.py
@@ -38,7 +38,10 @@ class Tutorial (object):
 
   def __init__ (self, connection):
     global config
-    print config
+    if DEBUGMODE is True:
+        print config
+    if VERBOSEMODE:
+        print ("__init__()")
     # Keep track of the connection to the switch so that we can
     # send it messages!
     self.connection = connection
@@ -51,41 +54,43 @@ class Tutorial (object):
     self.mac_to_port = {}
 
     # Insert flows from config list
-    global config
+    #global config
     for rule in config:
-      msg = of.ofp_flow_mod()
-      match = of.ofp_match()
-      if rule[srcIP] != 'any':
-        match.nw_src = rule[srcIP]
-      else:
-        match.nw_src = None
-      if rule[dstIP] != 'any':
-        match.nw_dst = rule[dstIP]
-      else:
-        match.nw_dst = None
-      if rule[srcPort] != 'any':
-        match.tp_src = rule[srcPort]
-      else:
-        match.tp_src = None
-      if rule[dstPort] != "any":  
-        match.tp_dst = rule[dstPort]
-      else:
-        match.tp_dst = None
-      msg.match = match
-      msg.hard_timeout = 0
-      msg.priority = 2
-      action = of.ofp_action_output(port = of.OFPP_CONTROLLER)  
-      msg.actions.append(action)
-      self.connection. send(msg)
+        msg = of.ofp_flow_mod()
+        match = of.ofp_match()
+        if rule[srcIP] != 'any':
+            match.nw_src = rule[srcIP]
+        else:
+            match.nw_src = None
+        if rule[dstIP] != 'any':
+            match.nw_dst = rule[dstIP]
+        else:
+            match.nw_dst = None
+        if rule[srcPort] != 'any':
+            match.tp_src = rule[srcPort]
+        else:
+            match.tp_src = None
+        if rule[dstPort] != "any":
+            match.tp_dst = rule[dstPort]
+        else:
+            match.tp_dst = None
+        msg.match = match
+        msg.hard_timeout = 0
+        msg.priority = 2
+        action = of.ofp_action_output(port = of.OFPP_CONTROLLER)
+        msg.actions.append(action)
+        self.connection. send(msg)
 
 
-      
+
   def resend_packet (self, packet_in, out_port):
     """
     Instructs the switch to resend a packet that it had sent to us.
     "packet_in" is the ofp_packet_in object the switch had sent to the
     controller due to a table-miss.
     """
+    if VERBOSEMODE:
+        print "resend_packet"
     msg = of.ofp_packet_out()
     msg.data = packet_in
 
@@ -96,8 +101,10 @@ class Tutorial (object):
     # Send message to switch
     self.connection.send(msg)
 
-  
+
   def check_config (self, fields):
+    if VERBOSEMODE:
+        print "check_config()"
     print fields
     global config, srcIP, srcPort, dstIP, dstPort
     flag = False
@@ -109,7 +116,7 @@ class Tutorial (object):
               print rule
               flag = True
               break
-    return flag    
+    return flag
 
 
   def act_like_switch (self, packet, packet_in):
@@ -118,8 +125,10 @@ class Tutorial (object):
     packet.src is (ethernet) source IP, packet.dst is (ethernet) dest IP
     packet_in.in_port is switch port it arrived on
     """
+    if(VERBOSEMODE):
+        print "act_like_switch()"
     print "Packet Type: ", packet.type
-    if packet.type == packet.ARP_TYPE: 
+    if packet.type == packet.ARP_TYPE:
       print "ARP"
     if packet.type == packet.IP_TYPE:
       ip_packet = packet.payload
@@ -130,23 +139,23 @@ class Tutorial (object):
           allowed = True
         else:
           allowed = False
-   
- 
+
+
     # Learn the port for the source MAC
     self.mac_to_port[packet.src] = packet_in.in_port
 
 
     if packet.dst in self.mac_to_port: # the port associated with the destination MAC of the packet is known:
-      # Send packet out the associated port
+        # Send packet out the associated port
       self.resend_packet(packet_in, self.mac_to_port[packet.dst])
 
       log.debug("Installing flow...from " + str(packet.src) + ", " + str(packet_in.in_port) + " to " + str(packet.dst) + ", " +  str(self.mac_to_port[packet.dst]))
 
       msg = of.ofp_flow_mod()
-      
+
       # Set fields to match received packet
       msg.match = of.ofp_match.from_packet(packet)
-      
+
       #< Set other fields of flow_mod (timeouts? buffer_id?) >
       msg.idle_timeout = 180
       msg.hard_timeout = 0
@@ -166,30 +175,39 @@ class Tutorial (object):
     """
     Handles packet in messages from the switch.
     """
+    if VERBOSEMODE:
+      print "_handle_PacketIn()"
     packet = event.parsed # This is the parsed packet data.
     if not packet.parsed:
       log.warning("Ignoring incomplete packet")
       return
     packet_in = event.ofp # The actual ofp_packet_in message.
     self.act_like_switch(packet, packet_in)
-  
 
-def parse_config(configuration): 
+
+def parse_config(configuration):
+  if VERBOSEMODE:
+      print "parse_config()"
   global config
   fin = open(configuration)
   for line in fin:
     rule = line.split()
     config.append(rule)
-  print config
+  if (False):
+    print config
 
 # to call, misc.of_tutorial --configuration=<path to config file>
 def launch (configuration=""):
   """
   Starts the component
   """
+  if VERBOSEMODE:
+      print "launch()"
   parse_config(configuration) #calls parseconfig method and passes string from command line
-  
+
   def start_switch (event):
+    if  VERBOSEMODE:
+        print("start_switch()")
     log.debug("Controlling %s" % (event.connection,))
     Tutorial(event.connection)
   core.openflow.addListenerByName("ConnectionUp", start_switch)
@@ -202,6 +220,6 @@ srcPort = 1
 dstIP = 2
 dstPort = 3
 
-
-
+DEBUGMODE=True # controls printing output like the parse_config output, etc.
+VERBOSEMODE=True # controls printing of the function names when they are called
 
